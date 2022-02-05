0a15ab883a16b2c994256000fe8ced3f07115e5e
polluxtroy3758 <2147396+polluxtroy3758@users.noreply.github.com>
2022-02-05 16:45:30 +0100

    feat: top level imports

--- src/pycounts_polluxtroy3758/__init__.py
+++ src/pycounts_polluxtroy3758/__init__.py
@@ -2,3 +2,6 @@
 from importlib.metadata import version
 
 __version__ = version("pycounts_polluxtroy3758")
+
+from pycounts_polluxtroy3758.plotting import plot_words  # noqa: F401
+from pycounts_polluxtroy3758.pycounts import count_words  # noqa: F401
